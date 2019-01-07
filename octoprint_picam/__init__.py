659c8bc8e724f61461b6580e77efc90ea337e0e8
Komoszek <komoszek10@gmail.com>
2019-01-07 21:41:02 +0100

    Dash Fix

---
+++ octoprint_picam/__init__.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class PiCamPlugin(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin,
+                       octoprint.plugin.AssetPlugin):
+	def on_after_startup(self):
+		self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+	def get_settings_defaults(self):
+		return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+	def get_template_configs(self):
+		return [
+			dict(type="navbar", custom_bindings=False),
+			dict(type="settings", custom_bindings=False)
+		]
+
+	def get_assets(self):
+		return dict(
+			js=["js/picam.js","js/http-live-player.js"],
+			css=["css/picam.css"],
+			less=["less/picam.less"]
+		)
+
+__plugin_name__ = "PiCam"
+__plugin_implementation__ = PiCamPlugin()
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "pi-cam"
+plugin_identifier = "picam"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_pi-cam"
+plugin_package = "octoprint_picam"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Pi-Cam"
+plugin_name = "OctoPrint-PiCam"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.0"
