91eee6c6b8a8cef31a346626da81b557374da36b
Komoszek <komoszek10@gmail.com>
2019-01-08 15:22:54 +0100

    One of milion tries

--- octoprint_picam/static/js/picam.js
+++ octoprint_picam/static/js/picam.js
@@ -10,45 +10,107 @@ $(function() {
 
 
         self.fullscreen = function() {
-            if(self.clickState === 0){
+          /*  if(self.clickState === 0){
               self.clickStateTimeout = setTimeout(function(){self.clickState = 0;},500);
               self.clickState = 1;
             } else {
-              clearTimeout(self.clickStateTimeout);
+              clearTimeout(self.clickStateTimeout);*/
               if(canvas.parentNode.classList.contains('fullscreen')){
                 canvas.parentNode.classList.remove('fullscreen');
               } else {
                 canvas.parentNode.classList.add('fullscreen');
               }
-              self.clickState = 0;
-            }
+              /*self.clickState = 0;
+            }*/
         };
+
+        self.toggleCamera = function() {
+
+        }
+
+        function dragElement(elmnt) {
+          var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+          elmnt.onmousedown = dragMouseDown;
+          var moveFlag = false;
+
+
+
+          function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+          }
+
+          function elementDrag(e) {
+            moveFlag = true;
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+
+            console.log(pos1, pos2, pos3, pos4);
+
+            elmnt.parentNode.style.width = max((elmnt.parentNode.clientWidth + pos3),((elmnt.parentNode.clientHeight + pos4)*(16/9)));
+            elmnt.parentNode.style.height = elmnt.parentNode.style.width* (9/16);
+
+          }
+
+          function closeDragElement() {
+            // stop moving when mouse button is released:
+
+            if(moveFlag === false){
+              if(elmnt.parentNode.classList.contains('minimized'))
+                elmnt.parentNode.classList.remove('minimized');
+              else
+                elmnt.parentNode.classList.add('minimized');
+            }
+
+            document.onmouseup = null;
+            document.onmousemove = null;
+          }
+        }
 
         // This will get called before the HelloWorldViewModel gets bound to the DOM, but after its depedencies have
         // already been initialized. It is especially guaranteed that this method gets called _after_ the settings
         // have been retrieved from the OctoPrint backend and thus the SettingsViewModel been properly populated.
         self.onBeforeBinding = function() {
           canvas = document.getElementById("pi-cam");
-          canvas.addEventListener('click', self.fullscreen, false);
+          canvas.addEventListener('dblclick', self.fullscreen, false);
+          //document.getElementById('pi-cam-button').addEventListener('click', self.toggleCamera, false);
+
+          dragElement(document.getElementById('pi-cam-button'));
+
           canvas.parentNode.classList.remove('hidden-feed');
 
-          canvas.parentNode.onresize = function(){
+          window.addEventListener('resize',function(){
             if(canvas.parentNode.classList.contains('fullscreen')){
               if(canvas.parentNode.clientWidth*9 > 16*canvas.parentNode.clientHeight){
-                canvas.setAttribute('height','100%');
-                canvas.setAttribute('width',null);
+                canvas.style.height = '100%';
+                canvas.style.width = 'auto';
               } else {
-                canvas.setAttribute('width','100%');
-                canvas.setAttribute('height',null);
+                canvas.style.height = 'auto';
+                canvas.style.width = '100%';
               }
             }
-          };
+          });
 
           var wsavc = new WSAvcPlayer(canvas, "webgl");
 
           var protocol = window.location.protocol === "https:" ? "wss:" : "ws:"
           wsavc.connect(protocol + '//' + document.location.hostname  + ':8080/video-stream');
         }
+
+
 
 
 
